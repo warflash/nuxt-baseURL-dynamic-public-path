4b33dd0d2d3db0a7d7ff23774e272a42c6a4eecc
warflash <nwiesinger67@gmail.com>
2023-03-20 20:48:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-baseURL-dynamic-public-path
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-9utpyr)
---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>
+      <div>Img 1 (&lt;img :src="'/img/' + 1 + '.webp'" /&gt;)</div>
+      <img :src="'/img/' + 1 + '.webp'" />
+    </div>
+    <div>
+      <div>Img 2 (&lt;img src="/img/2.webp" /&gt;)</div>
+      <img src="/img/2.webp" />
+    </div>
+  </div>
+</template>
